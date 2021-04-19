211743615d4c3b184572ee73c5b551151a24ae47
ginnnnnn <aginlo@hotmail.com>
2021-04-19 09:40:15 +0800

    add env setting

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+require("dotenv").config();
 const chokidar = require("chokidar");
 const path = require("path");
 const client = require("amqplib/callback_api");
+const internalIp = require("internal-ip");
+const express = require("express");
+const app = express();
 
-var q = "import_images";
+const {
+  BROKER_IP,
+  BROKER_PORT,
+  BROKER_QUEUE,
+  HOST_PORT,
+  WATCH_FOLDER,
+} = process.env;
+
+app.use(express.static(path.join(__dirname, "..", WATCH_FOLDER)));
 
 function bail(err) {
   console.error(err);
@@ -14,12 +26,14 @@ function publisher(conn, msg) {
   conn.createChannel(on_open);
   function on_open(err, ch) {
     if (err != null) bail(err);
-    ch.assertQueue(q);
-    ch.sendToQueue(q, Buffer.from(JSON.stringify(msg)));
+    ch.assertQueue(BROKER_QUEUE);
+    ch.sendToQueue(BROKER_QUEUE, Buffer.from(JSON.stringify(msg)));
   }
 }
 function formatTofolderPath(eventPath) {
-  let relativePath = eventPath.split(path.join(__dirname, ".."))[1];
+  let relativePath = eventPath.split(
+    path.join(__dirname, "..", WATCH_FOLDER)
+  )[1];
   //check if it is windows path and replace all \ to  /
   while (relativePath.includes("\\")) {
     relativePath = relativePath.replace("\\", "/");
@@ -27,60 +41,72 @@ function formatTofolderPath(eventPath) {
   relativePath = relativePath.slice(1);
   return relativePath;
 }
-client.connect("amqp://localhost", function (err, conn) {
-  if (err != null) bail(err);
 
-  // One-liner for current directory
-  chokidar
-    .watch(path.join(__dirname, "..", "images"))
-    .on("addDir", (eventPath) => {
-      let folderPath = formatTofolderPath(eventPath);
-      const msg = {
-        event: "addDir",
-        folderPath,
-      };
-      publisher(conn, msg);
-    })
-    .on("unlinkDir", (eventPath) => {
-      let folderPath = formatTofolderPath(eventPath);
-      const msg = {
-        event: "unlinkDir",
-        folderPath,
-      };
-      publisher(conn, msg);
-    })
-    .on("add", (eventPath) => {
-      let relativePath = formatTofolderPath(eventPath);
-      const filename = relativePath.split("/")[
-        relativePath.split("/").length - 1
-      ];
-      const folderPath = relativePath.split(filename)[0].slice(0, -1);
-      const msg = {
-        event: "add",
-        folderPath,
-        filename,
-      };
-      const fileType = filename.split(".")[filename.split(".").length - 1];
-      if (fileType !== "jpeg" && fileType !== "jpg" && fileType !== "png") {
-        return;
-      }
-      publisher(conn, msg);
-    })
-    .on("unlink", (eventPath) => {
-      let relativePath = formatTofolderPath(eventPath);
-      const filename = relativePath.split("/")[
-        relativePath.split("/").length - 1
-      ];
-      const folderPath = relativePath.split(filename)[0].slice(0, -1);
-      const msg = {
-        event: "unlink",
-        folderPath,
-        filename,
-      };
-      const fileType = filename.split(".")[filename.split(".").length - 1];
-      if (fileType !== "jpeg" && fileType !== "jpg" && fileType !== "png") {
-        return;
-      }
-      publisher(conn, msg);
-    });
+client.connect(
+  `amqp://${BROKER_IP}:${BROKER_PORT}`,
+  async function (err, conn) {
+    if (err != null) bail(err);
+    const ip = internalIp.v4.sync();
+    // One-liner for current directory
+    chokidar
+      .watch(path.join(__dirname, "..", WATCH_FOLDER))
+      .on("addDir", (eventPath) => {
+        let folderPath = formatTofolderPath(eventPath);
+        const msg = {
+          event: "addDir",
+          folderPath,
+          origin: ip,
+        };
+        publisher(conn, msg);
+      })
+      .on("unlinkDir", (eventPath) => {
+        let folderPath = formatTofolderPath(eventPath);
+        const msg = {
+          event: "unlinkDir",
+          folderPath,
+          origin: ip,
+        };
+        publisher(conn, msg);
+      })
+      .on("add", (eventPath) => {
+        let relativePath = formatTofolderPath(eventPath);
+        const filename = relativePath.split("/")[
+          relativePath.split("/").length - 1
+        ];
+        const folderPath = relativePath.split(filename)[0].slice(0, -1);
+        const msg = {
+          event: "add",
+          folderPath,
+          filename,
+          origin: ip,
+        };
+        const fileType = filename.split(".")[filename.split(".").length - 1];
+        if (fileType !== "jpeg" && fileType !== "jpg" && fileType !== "png") {
+          return;
+        }
+        publisher(conn, msg);
+      })
+      .on("unlink", (eventPath) => {
+        let relativePath = formatTofolderPath(eventPath);
+        const filename = relativePath.split("/")[
+          relativePath.split("/").length - 1
+        ];
+        const folderPath = relativePath.split(filename)[0].slice(0, -1);
+        const msg = {
+          event: "unlink",
+          folderPath,
+          filename,
+          origin: ip,
+        };
+        const fileType = filename.split(".")[filename.split(".").length - 1];
+        if (fileType !== "jpeg" && fileType !== "jpg" && fileType !== "png") {
+          return;
+        }
+        publisher(conn, msg);
+      });
+  }
+);
+
+app.listen(HOST_PORT, () => {
+  console.log(`server is running on ${HOST_PORT}`);
 });
